2d1b98d5efcf21e8820c9a6f7ffdd7363158cf81
Juan Antonio Suarez <jasrsir@gmail.com>
2016-10-07 00:07:28 +0200

    bug conocido a resolver

--- app/src/main/java/com/jasrsir/ejerciciostema1/EjercicioLibre_Activity.java
+++ app/src/main/java/com/jasrsir/ejerciciostema1/EjercicioLibre_Activity.java
@@ -13,11 +13,9 @@ import java.util.Random;
 public class EjercicioLibre_Activity extends AppCompatActivity {
 
     //region Variables
-    private int puntosJUno;
-    private int puntosJbanca;
     private int[] baraja;
     private Jugador computer;
-    private Jugador jugad1;
+    private Jugador jugador;
     private ImageView comp1;
     private ImageView comp2;
     private ImageView comp3;
@@ -31,9 +29,8 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
     private Button plantar;
     private TextView puntosJ;
     private TextView puntosC;
-    private Random rnd;
-    private double puntosCPartida;
-    private double puntosJpartida;
+    private Random rndJ;
+    private Random rndG;
     private int turno;
 
     //endregion
@@ -62,6 +59,7 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
                 plantar.setEnabled(false);
                 otra.setEnabled(false);
                 turnoPc();
+                jugador.plantarse = true;
                 start.setEnabled(true);
                 turno++;
                 break;
@@ -74,11 +72,10 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
     }
 
     private void turnoPc() {
-        if (puntosCPartida < 5) {
+        if (computer.puntos < 6.5 && !computer.plantarse) {
             echarCartaC();
 
-            if (puntosCPartida > 7.5) {
-
+            if (computer.puntos > 7.5) {
                 AlertDialog.Builder popup = new AlertDialog.Builder(EjercicioLibre_Activity.this);
                 popup.setTitle("HAS GANADO");
                 popup.setMessage("Has ganado, tu contrincante se ha pasado");
@@ -87,7 +84,8 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
                 plantar.setEnabled(false);
                 otra.setEnabled(false);
                 start.setEnabled(true);
-            } else if (puntosCPartida > puntosJpartida) {
+                jugador.ganadas++;
+            } else if (computer.puntos > jugador.puntos && jugador.plantarse) {
                 AlertDialog.Builder popup = new AlertDialog.Builder(EjercicioLibre_Activity.this);
                 popup.setTitle("Ha ganado el adversario");
                 popup.setMessage("Has perdido, tu contrincante tiene más puntos");
@@ -96,22 +94,17 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
                 plantar.setEnabled(false);
                 otra.setEnabled(false);
                 start.setEnabled(true);
-            }else if (puntosCPartida < puntosJpartida) {
-                AlertDialog.Builder popup = new AlertDialog.Builder(EjercicioLibre_Activity.this);
-                popup.setTitle("Has ganado");
-                popup.setMessage("tienes más puntos que tu adversario");
-                popup.setPositiveButton("Yo sabia que era el mejor!", null);
-                popup.show();
-                plantar.setEnabled(false);
-                otra.setEnabled(false);
-                start.setEnabled(true);
+            }else if (computer.puntos < jugador.puntos && jugador.plantarse) {
+                turnoPc();
             }
 
-        }
+        }else
+            computer.plantarse = true;
     }
 
+    //Método que saca una carta al Computer, y tirar si se ha plantado el jugador
     private void echarCartaC() {
-        int cartaC = rnd.nextInt(baraja.length);
+        int cartaC = rndG.nextInt(baraja.length);
         if (turno == 1)
             comp2.setImageResource(baraja[cartaC]);
         if (turno == 2)
@@ -119,16 +112,15 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
         if (turno == 3) {
             comp4.setImageResource(baraja[cartaC]);
         }
-        puntosCPartida += sacarValor(cartaC);
+        computer.puntos += sacarValor(cartaC);
 
-
-
-
+        if (jugador.plantarse)
+            turnoPc();
     }
 
     //metodo que echa una carta al jugador
     private void echarCarta() {
-        int cartaJ = rnd.nextInt(baraja.length);
+        int cartaJ = rndJ.nextInt(baraja.length);
         if (turno == 1)
             jug2.setImageResource(baraja[cartaJ]);
         if (turno == 2)
@@ -136,37 +128,36 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
         if (turno == 3)
             jug4.setImageResource(baraja[cartaJ]);
 
-        puntosJpartida += sacarValor(cartaJ);
+        jugador.puntos += sacarValor(cartaJ);
 
-        if (puntosJpartida > 7.5) {
-            plantar.setEnabled(false);
-            otra.setEnabled(false);
+        if (jugador.puntos > 7.5) {
             AlertDialog.Builder popup = new AlertDialog.Builder(EjercicioLibre_Activity.this);
             popup.setTitle("Te has pasado");
             popup.setMessage("Si te sirve de consuelo.. has perdido");
             popup.setPositiveButton("bueno... la próxima será.", null);
             popup.show();
-            start.setEnabled(true);
-        } else if (puntosJpartida == 7.5) {
-            plantar.setEnabled(false);
-            otra.setEnabled(false);
+            computer.ganadas++;
+            fin();
+        } else if (jugador.puntos == 7.5) {
             AlertDialog.Builder popup = new AlertDialog.Builder(EjercicioLibre_Activity.this);
             popup.setTitle("¡¡7 y medio!!");
             popup.setMessage("Has ganado por goleadaaa");
             popup.setPositiveButton("Perfect", null);
             popup.show();
-            start.setEnabled(true);
+            jugador.ganadas++;
+            fin();
         }
+
     }
 
     //método que simula la primera tirada
     private void echarInicial() {
-        int cartaP = rnd.nextInt(baraja.length);
+        int cartaP = rndG.nextInt(baraja.length);
         comp1.setImageResource(baraja[cartaP]);
-        puntosCPartida = sacarValor(cartaP);
-        int cartaJ = rnd.nextInt(baraja.length);
+        computer.puntos = sacarValor(cartaP);
+        int cartaJ = rndJ.nextInt(baraja.length);
         jug1.setImageResource(baraja[cartaJ]);
-        puntosJpartida = sacarValor(cartaJ);
+        jugador.puntos = sacarValor(cartaJ);
         turno++;
     }
 
@@ -189,11 +180,9 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
 
     }
 
-
-
     //Metodo que inicia la baraja, jugadores y demás variables
     private void start() {
-        rnd = new Random();
+        rndJ = new Random();
         baraja = new int[] {R.drawable.b1, R.drawable.b2, R.drawable.b3, R.drawable.b4, R.drawable.b5, R.drawable.b6,
                 R.drawable.b7, R.drawable.b10, R.drawable.b11, R.drawable.b12,
                 R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4, R.drawable.c5, R.drawable.c6,
@@ -202,10 +191,14 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
                 R.drawable.o7, R.drawable.o10, R.drawable.o11, R.drawable.o12,
                 R.drawable.e1, R.drawable.e2, R.drawable.e3, R.drawable.e4, R.drawable.e5, R.drawable.e6,
                 R.drawable.e7, R.drawable.e10, R.drawable.e11, R.drawable.e12};
-        if (jugad1 == null && computer == null) {
-            jugad1 = new Jugador();
+        if (jugador == null && computer == null) {
+            jugador = new Jugador();
             computer = new Jugador();
+        } else {
+            computer.puntos = 0;
+            jugador.puntos = 0;
         }
+
         comp1.setImageResource(R.drawable.cardback);
         comp2.setImageResource(R.drawable.cardback);
         comp3.setImageResource(R.drawable.cardback);
@@ -214,52 +207,51 @@ public class EjercicioLibre_Activity extends AppCompatActivity {
         jug2.setImageResource(R.drawable.cardback);
         jug3.setImageResource(R.drawable.cardback);
         jug4.setImageResource(R.drawable.cardback);
-        puntosCPartida = 0;
-        puntosJpartida = 0;
+        rndG = new Random();
         turno = 0;
     }
 
     //Metodo que reinicia la partida
     private void fin() {
-        baraja = null;
-        start();
-
-
+        puntosJ.setText(jugador.ganadas);
+        puntosC.setText(computer.ganadas);
+        plantar.setEnabled(false);
+        otra.setEnabled(false);
+        start.setEnabled(true);
     }
 
-    //Clase Jugador para los 2 jugadores
+    //Clase Jugador para los 2 jugadores con un comportamiento
     private class Jugador {
         protected int ganadas;
-
+        protected double puntos;
+        protected boolean plantarse;
         public Jugador() {
             ganadas = 0;
+            puntos = 0;
+            plantarse = false;
         }
 
     }
-
+    //Método que saca el valor obtenido de la carta.
     private double sacarValor(int carta){
 
         double valor = 0;
 
-        if (carta == baraja[0] || carta == baraja[10] || carta == baraja[20] || carta == baraja[30])
+        if (carta == 0 || carta == 10 || carta == 20 || carta == 30)
             valor = 1;
-        else if (carta == baraja[1] || carta == baraja[11] || carta == baraja[21] || carta == baraja[31])
+        else if (carta == 1 || carta == 11 || carta == 21 || carta == 31)
             valor = 2;
-        else if (carta == baraja[2] || carta == baraja[12] || carta == baraja[22] || carta == baraja[32])
+        else if (carta == 2 || carta == 12 || carta == 22 || carta == 32)
             valor = 3;
-        else if (carta == baraja[3] || carta == baraja[13] || carta == baraja[23] || carta == baraja[33])
+        else if (carta == 3 || carta == 13 || carta == 23 || carta == 33)
             valor = 4;
-        else if (carta == baraja[4] || carta == baraja[14] || carta == baraja[24] || carta == baraja[34])
+        else if (carta == 4 || carta == 14 || carta == 24 || carta == 34)
             valor = 5;
-        else if (carta == baraja[5] || carta == baraja[15] || carta == baraja[25] || carta == baraja[35])
+        else if (carta == 5 || carta == 15 || carta == 25 || carta == 35)
             valor = 6;
-        else if (carta == baraja[6] || carta == baraja[16] || carta == baraja[26] || carta == baraja[36])
+        else if (carta == 6 || carta == 16 || carta == 26 || carta == 36)
             valor = 7;
-        else if (carta == baraja[7] || carta == baraja[17] || carta == baraja[27] || carta == baraja[37])
-            valor = 0.5;
-        else if (carta == baraja[8] || carta == baraja[18] || carta == baraja[28] || carta == baraja[38])
-            valor = 0.5;
-        else if (carta == baraja[9] || carta == baraja[19] || carta == baraja[29] || carta == baraja[39])
+        else
             valor = 0.5;
 
         return valor;
